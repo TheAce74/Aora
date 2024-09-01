4bf20f798ca7e9a7d8caa43db58e667160b74d92
Chisom Udonsi <udonsichisom02@gmail.com>
2024-09-01 08:04:25 +0100

    feat: include app assets and nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,13 @@
 import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function index() {
   return (
-    <View style={styles.container}>
-      <Text>Aora Home</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Aora Home</Text>
       <Link href="/profile" style={{ color: "red" }}>
         Go to profile
       </Link>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
